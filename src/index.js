b546110b9e022fa65bb81b7a0243e1f8ae5e0d52
RJ Salvador <salvador.workshop@gmail.com>
2025-06-10 19:48:34 -0400

    small api tweak

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const coreModule = require('./core');
 const utilsModule = require('./utils');
 const detailsModule = require('./details');
+const stdSpecs = require('sw-jscad-std-specs');
 
-const init = ({ stdSpecs, lib }) => {
+const init = ({ lib }) => {
     const swJscad = {
         core: { ...stdSpecs, ...coreModule.init({ stdSpecs, lib }) },
     }
