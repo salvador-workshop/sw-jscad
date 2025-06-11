943d95d7e671f04ca80a0368a01d3a4a131304c3
RJ Salvador <salvador.workshop@gmail.com>
2025-06-10 20:10:53 -0400

    burgfurx

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const coreModule = require('./core');
 const utilsModule = require('./utils');
 const detailsModule = require('./details');
-const stdSpecs = require('sw-jscad-std-specs');
 
 const init = ({ lib }) => {
+    const stdSpecs = require('sw-jscad-std-specs').init({ lib });
     const swJscad = {
         core: { ...stdSpecs, ...coreModule.init({ stdSpecs, lib }) },
     }
