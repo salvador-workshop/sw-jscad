28bdd3a46423d4637549e5837dc3d4c296d2883a
RJ Salvador <salvador.workshop@gmail.com>
2025-07-26 16:53:10 -0400

    hacky cleans

--- src/models/profiles/index.js
+++ src/models/profiles/index.js
@@ -3,11 +3,11 @@
 const profilesInit = ({ lib, swLib }) => {
     const profiles = {
         ...require('./profiles').init({ lib, swLib }),
-        connections: require('./connections').init({ lib, swLib }),
-        edge: require('./edge').init({ lib, swLib }),
+        // connections: require('./connections').init({ lib, swLib }),
+        // edge: require('./edge').init({ lib, swLib }),
         foils2d: require('./foils-2d').init({ lib, swLib }),
         mesh2d: require('./mesh-2d').init({ lib, swLib }),
-        reinforcement: require('./reinforcement').init({ lib, swLib }),
+        // reinforcement: require('./reinforcement').init({ lib, swLib }),
         text2d: require('./text-2d').init({ lib, swLib }),
     }
 
